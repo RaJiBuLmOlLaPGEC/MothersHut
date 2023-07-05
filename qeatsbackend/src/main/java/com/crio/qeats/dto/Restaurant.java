328a059b143f6294b99942070520723065b22fd6
Rajibul <rajibulmolla5@gmail.com>
2023-07-05 17:31:11 +0530

    ME_QEATS_V2_MODULE_SERIALIZATION

--- qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
+++ qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
@@ -32,8 +32,17 @@ import lombok.NoArgsConstructor;
 //    "South Indian"
 //  ]
 // }
-
+@Data
 public class Restaurant {
 
+    private String restaurantId;
+    private String name;
+    private String city;
+    private String imageUrl;
+    private double latitude;
+    private double longitude;
+    private String opensAt;
+    private String closesAt;
+    private List<String> attributes;
 }
 
